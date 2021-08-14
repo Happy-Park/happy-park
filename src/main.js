349fb5fcbeb575e7c79eedf7d29b7b43908ea223
GrodrZ <fernandogroder@gmail.com>
2021-08-13 21:24:00 -0300

    init

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const onlineStatusWindow = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+  }
+  })
+  
+  onlineStatusWindow.loadFile('./pages/home.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
