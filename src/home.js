2b89541dc616d69f664ff88f73b36e4db0a6e161
Spacewiorblue <francisco.cavasan12@gmail.com>
2021-09-09 18:11:17 -0300

    add previsao simples

--- src/home.js
+++ src/home.js
@@ -1,34 +1,17 @@
-var navbar = document.getElementById("navbar");
-let buttons = navbar.querySelectorAll("button");
+let wheater = document.getElementById('weather')
+let forecast = document.getElementById('forecast')
+let apiKey  = 'baddcdc6'
+let url = `https://api.hgbrasil.com/weather?format=json-cors&key=${apiKey}&city_name=Lajeado,RS`
 
-document.addEventListener(
-  "scroll",
-  function () {
-    var scroll = window.scrollY;
-    if (scroll > 0) {
-      navbar.style.backgroundColor = "#ffa";
-      buttons.forEach((element) => {
-        element.style.backgroundColor = "#999";
-      });
-    } else if (scroll === 0) {
-      navbar.style.backgroundColor = "transparent";
-      buttons.forEach((element) => {
-        element.style.backgroundColor = "transparent";
-      });
-    }
-  },
-  { passive: true }
-);
 
-buttons.forEach((element) => {
-  element.addEventListener("mouseenter", function () {
-    element.style.backgroundColor = "rgb(255,0,255)";
-  });
-  element.addEventListener("mouseleave", function () {
-    if (window.scrollY > 0) {
-      element.style.backgroundColor = "#999";
-    } else {
-      element.style.backgroundColor = "transparent";
-    }
-  });
-});
+let request = new XMLHttpRequest();
+request.open('GET', url)
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var response = request.response.results;
+wheater.innerText +=' ' + response.temp + 'C, ' + response.description + '. Hora: ' + response.time
+forecast.innerText += ' miníma:' + response.forecast[1].min + ' C. Máxima:' + response.forecast[1].max + ' C. Descrição: ' + response.forecast[1].description
+console.log(response)
+}
