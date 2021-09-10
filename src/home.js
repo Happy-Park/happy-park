c4c5596fb0e0cf982acbf7ef8335d1be23c9f9b9
francisco <francisco.cavasan12@gmail.com>
2021-09-10 20:38:56 -0300

    delete old files

--- src/home.js
+++ src/home.js
@@ -7,8 +7,8 @@ let url;
 let request = new XMLHttpRequest();
 
 cepBtn.addEventListener("click", () => {
-  wheater.innerText = "Temperatura em "
-  forecast.innerText = "Previsão para amanhã:"
+  wheater.innerText = "Temperatura em ";
+  forecast.innerText = "Previsão para amanhã:";
   let cep = document.getElementById("cep");
   if (cep.value != null) {
     request.open("GET", `https://brasilapi.com.br/api/cep/v2/${cep.value}`);
@@ -20,19 +20,17 @@ cepBtn.addEventListener("click", () => {
       let cityName = info.city;
       let uf = info.state;
       url = `https://api.hgbrasil.com/weather?format=json-cors&key=${apiKey}&city_name=${cityName}`;
-      request.open('GET', url)
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-  var response = request.response.results;
-wheater.innerText +=`${cityName}: ` + response.temp + 'C, ' + response.description + '. Hora: ' + response.time
-forecast.innerText += ' miníma:' + response.forecast[1].min + ' C. Máxima:' + response.forecast[1].max + ' C. Descrição: ' + response.forecast[1].description
-console.log(response)
-}
+      request.open("GET", url);
+      request.responseType = "json";
+      request.send();
+      request.onload = function () {
+        var response = request.response.results;
+        wheater.innerText +=
+          ` ${cityName}: ` + response.temp +"C, " +response.description +". Hora: " + response.time;
+        forecast.innerText +=" miníma:" + response.forecast[1].min +" C. Máxima:" + response.forecast[1].max + " C. Descrição: " +
+          response.forecast[1].description;
+        console.log(response);
+      };
     };
   }
 });
-
-
-
-
