ac0a13dd350d18c32895ee4e7bff68f33fbe140b
FeGroders <fernandogroder@gmail.com>
2021-09-13 18:46:04 -0300

    feat: alt img conforme clima

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 let cepBtn = document.getElementById("get-cep");
 let wheater = document.getElementById("weather");
-let forecast = document.getElementById("forecast");
+let img = document.getElementById("img-weather");
 let apiKey = process.env.API_KEY_FORECAST;
 let cityName;
 let url;
@@ -27,6 +27,27 @@ let info = request.response;
   request.send();
   request.onload = function () {
     var response = request.response.results;
+
+    //Ajeita imagem conforme o clima
+    if (parseInt(response.condition_code) in [0,1,3,4,37,38,39,47]){
+      img.src = "../assets/img/weather/tempestade.png";
+    }
+    else if (parseInt(response.condition_code) in [9,11,12,35,40,45]){
+      img.src = "../assets/img/weather/chuva.png";
+    }
+    else if (parseInt(response.condition_code) in [2,15,21,23,24]){
+      img.src = "../assets/img/weather/vento.png";
+    }
+    else if (parseInt(response.condition_code) in [13,15,16,17,18,41,42,43,46]){
+      img.src = "../assets/img/weather/inverno.png";
+    }
+    else if (parseInt(response.condition_code) in [25,27,31,32,33,44]){
+      img.src = "../assets/img/weather/nuvem.png";
+    }
+    else {
+      img.src = "../assets/img/weather/nublado.png";
+    }
+
     wheater.innerText +=
       ` ${cityName}: ` + response.temp +"°C, " +response.description +". Hora: " + response.time;
     // forecast.innerText +=" Miníma:" + response.forecast[1].min +" °C Máxima:" + response.forecast[1].max + " °C. Descrição: " +
