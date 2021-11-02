075487d407ce8b29346ce2c87623c7b49000b97a
GrodrZ <fernandogroder@gmail.com>
2021-11-02 16:00:33 -0300

    feat: add localizacao mapa

--- src/home_user.js
+++ src/home_user.js
@@ -8,4 +8,11 @@ new glide('.glide', {
     
 }).mount()
 
+var map = L.map('map').setView([-29.4446788,-51.9583461], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-29.4446788,-51.9583461]).addTo(map).bindPopup('Happy Park');
 
